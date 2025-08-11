a72b89f74f9b680107af2953ce10246a7f12059f
Next_level_mark <marksimiyu002@gmail.com>
2025-08-11 19:00:42 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+package com.destinydate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinyDateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DestinyDateApplication.class, args);
+    }
+
+}
